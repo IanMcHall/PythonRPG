dcb53fe73b2883f72c242b541dac6d1c57f53b86
Ian <ianmchall@gmail.com>
2024-06-08 21:51:40 -0400

    Added basic battle logic

--- ConsoleQuest.py
+++ ConsoleQuest.py
@@ -22,13 +22,38 @@ battle_menu = {
     "2": "Assess",
 }
 
-
+goblin_damage_done = goblin.health + goblin.defense - player_character.attack 
 
 print(f"You encounter a {goblin.name}!\n")
-print("What will you do?")
-for option_number, option in battle_menu.items():
-    print(f"{option_number}. {option}")
-choice = input()
-while choice not in battle_menu:
-    choice = input("Select a valid option\n")
+
+while goblin.health > 0:
+
+
+    print("What will you do?\n")
+    for option_number, option in battle_menu.items():
+        print(f"{option_number}. {option}")
+    choice = input()
+
+    while choice not in battle_menu:
+        print("Select a valid option")
+        for option_number, option in battle_menu.items():
+            print(f"{option_number}. {option}")
+            choice = input()
+    
+
+    if choice == "1":
+        print(f"\nYou attack the {goblin.name}!\n")
+        goblin.health -= goblin_damage_done
+        print(f"The {goblin.name} takes {goblin_damage_done} damage!\n")
+        if goblin.health <= 0:
+            print("The enemy is defeated!")
+            break
+    elif choice == "2":
+        print(f"\nYou assess the {goblin.name}")
+        print(" ")
+        print(f"{goblin.name.capitalize()}: {goblin.bio}\n HP: {goblin.health}\n\n")
+        
+
+
+
     
